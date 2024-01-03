2c2493cca3af5edd5c22cedda45464b6b38115c9
José Ferreira de Moura Júnior <85230820+JoseJuniokfm@users.noreply.github.com>
2024-01-02 21:47:18 -0300

    adicionando swagger

--- controllers/processoControllers.js
+++ controllers/processoControllers.js
@@ -1,3 +1,204 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Processos
+ *   description: Rotas relacionadas a processos
+ */
+
+/**
+ * @swagger
+ * /processos:
+ *   get:
+ *     summary: Lista todos os processos
+ *     tags: [Processos]
+ *     responses:
+ *       200:
+ *         description: Retorna a lista de todos os processos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Processo'
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ */
+
+/**
+ * @swagger
+ * /processos:
+ *   post:
+ *     summary: Cria um novo processo
+ *     tags: [Processos]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProcessoInput'
+ *     responses:
+ *       201:
+ *         description: Processo criado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Processo'
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ */
+
+/**
+ * @swagger
+ * /processos/{id}:
+ *   get:
+ *     summary: Obtém um processo pelo ID
+ *     tags: [Processos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do processo a ser obtido
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Retorna os detalhes do processo
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Processo'
+ *       404:
+ *         description: Processo não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ */
+
+/**
+ * @swagger
+ * /processos/{id}:
+ *   put:
+ *     summary: Atualiza um processo pelo ID
+ *     tags: [Processos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do processo a ser atualizado
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ProcessoInput'
+ *     responses:
+ *       200:
+ *         description: Processo atualizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Processo'
+ *       404:
+ *         description: Processo não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ */
+
+/**
+ * @swagger
+ * /processos/{id}:
+ *   delete:
+ *     summary: Exclui um processo pelo ID
+ *     tags: [Processos]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID do processo a ser excluído
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Processo excluído com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensagem de sucesso
+ *       404:
+ *         description: Processo não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ *       500:
+ *         description: Erro interno do servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Mensagem de erro
+ */
+
 const Processo = require('../models/processoModel');
 
 async function listarProcessos(req, res) {
@@ -33,7 +234,7 @@ async function obterProcesso(req, res) {
     try {
       const { id } = req.params;
       const processo = await Processo.findOne({
-        where: { id, dados_denunciante },
+        where: { id },
       });
   
       if (!processo) {
