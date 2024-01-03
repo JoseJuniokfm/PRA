10dea5b001c47edb5ed1d92047c906e2ab98f408
José Ferreira de Moura Júnior <85230820+JoseJuniokfm@users.noreply.github.com>
2024-01-02 21:48:35 -0300

    adicionando swagger

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const authRoutes = require('./routes/authRoutes');
 const processoRoutes = require('./routes/processoRoutes');
 const reclamacaoRoutes = require('./routes/reclamacaoRoutes');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/auth', authRoutes);
 app.use('/processo', processoRoutes);
 app.use('/reclamacao', reclamacaoRoutes);
